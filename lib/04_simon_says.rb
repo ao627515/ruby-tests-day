91542ed237ce6781c8bfcb4cc4ba91fe436c5f0d
ao627515 <ao627515@gmail.com>
2024-07-01 18:23:36 +0000

    test "titleize" success

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -17,3 +17,18 @@ end
 def first_word(str)
     str.split(" ").first
 end
+
+def titleize(str)
+    str.split.map(&:capitalize).join(' ')
+end
+
+def titleize(title)
+    little_words = ["and", "the"]
+    title.capitalize.split.map { |word|
+      if little_words.include?(word)
+        word
+      else
+        word.capitalize
+    end
+    }.join(' ')
+end
